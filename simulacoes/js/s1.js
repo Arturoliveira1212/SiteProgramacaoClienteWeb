ef68b5fa35803a72135a5ccc98e4c3eecb91802a
Arturoliveira1212 <arturalvesdeoliveira28@gmail.com>
2020-12-10 15:13:57 -0300

    Consertei um erro de  exibição na simulação 1 de química.

--- simulacoes/js/s1.js
+++ simulacoes/js/s1.js
@@ -1,26 +1,41 @@
-var botao1 = document.getElementById('botao1');
-botao1.onclick = function transformação(){
+var botao = document.getElementById('botao');
+var limpar = document.getElementById('limpar')
+var select = document.getElementById('corr');
+		
+botao.onclick = function transformação()
+{
+	var inputt = document.getElementById('valor').value;
+	if(inputt == '' || inputt<=0)
+	{
+		alert("Insira um valor válido.");
+	}
+	else 
+	{
+		if(select.value == '2')
+		{
+			var massa = inputt;
+			massa = parseFloat(massa);
+			var gramas = 23;
+			var resultado = (massa/gramas);
+			resultado = resultado.toFixed(2);
+			var resultadoCompleto = "23 g - 1 mol <br>"  + massa + " g - X mol(s) <br><br>" + " 23 . X = 1 . " + massa + " <br> X = " + massa + "/23 <br> " + "X = " + resultado + " <br><br> " + massa + " g Na = " + resultado + " mol(s) Na";
+			document.getElementById('resultado').innerHTML = resultadoCompleto;
+		}
+		else if(select.value == '1')
+		{
+			var mol = inputt;
+			mol = parseFloat(mol);
+			var gramas = 23;
+			var resultado = (gramas*mol);
+			resultado = resultado.toFixed(2);
+			var resultadoCompleto = "23 g - 1 mol <br> X g - " + mol + " mol <br><br> 1 . X = 23 . " + mol + " <br> X = " + gramas*mol + "/1 <br> " + "X = " + resultado + " g <br><br> " + mol + " mol Na = " + resultado + " g Na";
+			document.getElementById("resultado").innerHTML = resultadoCompleto;
+		}
+	}
 
-	var massa = document.getElementById('massa').value;
-	massa = parseFloat(massa);
-	var gramas = 23;
-	var resultado = (massa/gramas);
-	resultado = resultado.toFixed(2);
-	var resultadoCompleto = "1 mol - 23 g <br> X mol - " + massa +" g <br> <br> 23 . X = 1 . " + massa + " <br> X = " + massa + "/" + " 23 <br> X = " + resultado + " mol(s). <br><br> " + massa + " g = " + resultado + " mol(s)";
-	document.getElementById('resultado').innerHTML = resultadoCompleto;
-
-
-}
-
-var botao2 = document.getElementById('botao2');
-botao2.onclick = function transformação2(){
-
-	var mol = document.getElementById('mol').value;
-	mol = parseFloat(mol);
-	var gramas = 23;
-	var resultado = (gramas*mol);
-	resultado = resultado.toFixed(2);
-	var resultadoCompleto = "23 g - 1 mol <br> X g - " + mol + " mol <br><br> 1 . X = 23 . " + mol + " <br> X = " + gramas*mol + "/1 <br> " + "X = " + resultado + " g <br><br> " + mol + " mol = " + resultado + " g";
-	document.getElementById("resultado2").innerHTML = resultadoCompleto;
-
+	limpar.onclick = function limpa()
+	{
+		document.getElementById('resultado').innerHTML = '';
+		document.getElementById('valor').value = '';
+	}
 }
