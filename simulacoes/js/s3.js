b8ec1cbdcab175ad1a15d2ad4bdc784124f0c4f1
Arturoliveira1212 <arturalvesdeoliveira28@gmail.com>
2020-12-14 12:08:26 -0300

    Atualizei a simulação 3.

--- simulacoes/js/s3.js
+++ simulacoes/js/s3.js
@@ -19,20 +19,24 @@ botao.onclick = function retorna()
         var y1 = y/22.4;
         var r = inputto2 - x1;
         var r2 = inputth2 - y1;
+        var massa1 = inputto2 + y1;
+        var massa = inputth2 + x1;
         x = x.toFixed(2);
         x1 = x1.toFixed(2);
         y = y.toFixed(2);
         y1 = y1.toFixed(2);
         r = r.toFixed(2);
         r2 = r2.toFixed(2);
+        massa = massa.toFixed(2);
+        massa1 = massa1.toFixed(2);
         if(x1<inputto2)
         {
-            var excesso = "44,8L H2 - 22,4L O2 <br>" + inputth2 + "L H2 - XL O2 <br><br> 44,8 . X = " + x +"<br> X = " + x1 + "<br><br>Para " + inputth2 + "L H2 precisa-se de " + x1 + "L O2<br> " + inputto2 +" - " + x1 + " = " + r + "<br><br>O2 está " + r + " L em excesso<br> O agente limitante é o H2";
+            var excesso = "44,8L H2 - 22,4L O2 <br>" + inputth2 + "L H2 - XL O2 <br><br> 44,8 . X = " + x +"<br> X = " + x1 + "<br><br>Para " + inputth2 + "L H2 precisa-se de " + x1 + "L O2<br> " + inputto2 +" - " + x1 + " = " + r + "<br><br>O2 está " + r + " L em excesso<br> O agente limitante é o H2 <br> O vulume de H2O formado é de " + massa + "L";
 
         }
         else if(x1>inputto2)
         {
-            var excesso = "44,8L H2 - 22,4L O2 <br> XL O2" + inputto2 + "L O2 <br><br> 22,4 . X = " + y +"<br> X = " + y1 + "<br><br>Para " + inputto2 + "L O2 precisa-se de " + y1 + "L H2<br> " + inputth2 +" - " + y1 + " = " + r2 + "<br><br>H2 está " + r2 + " L em excesso<br> O agente limitante é o O2";
+            var excesso = "44,8L H2 - 22,4L O2 <br> XL O2" + inputto2 + "L O2 <br><br> 22,4 . X = " + y +"<br> X = " + y1 + "<br><br>Para " + inputto2 + "L O2 precisa-se de " + y1 + "L H2<br> " + inputth2 +" - " + y1 + " = " + r2 + "<br><br>H2 está " + r2 + " L em excesso<br> O agente limitante é o O2 <br> O volume de H2O formade é de " + massa1 + "L";
         }
         else if(x1 == inputto2)
         {
