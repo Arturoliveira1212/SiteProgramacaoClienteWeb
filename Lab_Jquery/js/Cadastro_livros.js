1137e34ff6093c8a20f77d416875f0278cc09a2d
Arturoliveira1212 <arturalvesdeoliveira28@gmail.com>
2021-01-19 20:27:42 -0300

    Realizei o laboratório Jquery.

---
+++ Lab_Jquery/js/Cadastro_livros.js
@@ -0,0 +1,64 @@
+$(function(){
+
+    let estante = new Array();
+    $("#add_acervo").click(function(){
+        let titulo = $("#titulo").val();
+        let descricao = $("#descricao").val();
+        let genero = $("#genero").val();
+        let autor =  $("#autor").val();
+        let acervo = $("#acervo");
+        let obj_tr = $("<tr>");
+        obj_tr.append($("<td>").text(titulo));
+        obj_tr.append($("<td>").text(descricao));
+        obj_tr.append($("<td>").text(genero));
+        obj_tr.append($("<td>").text(autor));
+        let obj_img = $("<img>").attr("src", "../images/delete1.png");
+        obj_img.addClass("lixeira");
+        let obj_link = $("<a>").attr("href" , "#");
+        obj_link.append(obj_img);
+        obj_tr.append(obj_link);
+        acervo.append(obj_tr);
+        let livro = {
+            "titulo":  titulo,
+            "descricao": descricao,
+            "genero": genero,
+            "autor": autor
+        };
+        estante.push(livro);
+        $("#titulo").val("");
+        $("#descricao").val("");
+        $("#autor").val("");
+        let autores = $("#autores");
+        let obj_type_search = $("<option>").text(autor);
+        autores.append(obj_type_search);
+    })
+    
+    $("#acervo").on("click", "img", function(){
+        let tam = estante.length;
+        texto1 = estante[0].titulo + estante[0].descricao + estante[0].genero + estante[0].autor;
+        let texto_selecionado = $(this).parents("#acervo tr").text();
+        if(texto1 == texto_selecionado)
+        {
+            estante.splice(0,1);
+        }
+        else
+        {
+            let i;
+            for(i=1;i<tam;i++)
+            {
+                let texto = estante[i].titulo + estante[i].descricao + estante[i].genero + estante[i].autor;
+                if(texto == texto_selecionado)
+                {
+                    estante.splice(i,1);
+                }
+            }
+        }
+        $(this).parents("#acervo tr").remove();
+    })
+    $("#salvar").click(function(){
+    
+        $("input[name=acervo_post]").val(JSON.stringify(estante)).val();
+    console.log(estante)
+    })
+
+});
---
+++ Lab_Jquery/js/Glossario_online.js
@@ -0,0 +1,56 @@
+let glossario = [
+    {
+        "id":1,
+        "termo": "W3C",
+        "definicao": "World Wide Web Consortium – escritório responsável por desenvolver normas técnicas para a Internet.",
+        "autor": "José Borges.",
+        "dataCriacao": "2015-04-01"
+    },
+    {
+        "id":2,
+        "termo": "HTML",
+        "definicao": "HyperText Markup Language – linguagem de marcação utilizada para estruturar páginas web.",
+        "autor": "Pedro Silva.",
+        "dataCriacao": "2017-05-12"
+    },
+    {
+        "id":3,
+        "termo": "CSS",
+        "definicao": "Cascade Style Sheet – Folhas de estilo utilizadas para configurar a visualização de  páginas web.",
+        "autor": "Maria Machado.",
+        "dataCriacao": "2018-10-11"
+    }
+];
+
+console.log(glossario[0].termo);
+$(function(){
+
+    $("#link").click(function(){
+        $(this).text("Termos cadastrados: clique na definição desejada.");
+        for(i=0;i<glossario.length;i++)
+        {
+            let obj_lista1 = $("<li>");
+            let termo1 = glossario[i].termo;
+            obj_lista1.append(termo1);
+            let obj_ul = $("#lista");
+            obj_ul.append(obj_lista1);
+        }
+    })
+
+    $("#lista").on("click", "li", function(){
+        
+        for(i=0;i<glossario.length;i++)
+        {
+            if($(this).text() == glossario[i].termo)
+            {
+                $("#definicao").text("Definição");   
+                $("#termo").text(glossario[i].termo);
+                $("#descricao").text(glossario[i].definicao);
+                let data =  glossario[i].dataCriacao.substring(8,10) + "/" + glossario[i].dataCriacao.substring(5,7) + "/" + glossario[i].dataCriacao.substring(0,4);
+                $("#data_criacao").text("Data de criação: " + data);
+                $("#autor").text("Autor: " + glossario[i].autor);
+            }
+        }
+    })
+
+});
