ff9a3e1e7dc89c6adee665403d18f93a941be329
Arturoliveira1212 <arturalvesdeoliveira28@gmail.com>
2021-06-01 14:39:57 -0300

    Coloquei o conteúdo de cinetica e termodinamica

--- Lab_ECMA_SCRIPT6_2/js/exercicio2.js
+++ Lab_ECMA_SCRIPT6_2/js/exercicio2.js
@@ -3,7 +3,7 @@ $(function(){
     $("#botao").click(function(){
         let salario = $("#salario").val();
         console.log(salario);
-        $("#resultado").text("Resultado = " + impos(salario).toFixed(2) + " reais.");
+        $("#resultado").text("Resultado = " + impos(salario));
     })
 
     $("#limpar").click(function(){
@@ -20,19 +20,19 @@ $(function(){
         }
         if(salario > 1903.99 && salario <= 2826.65)
         {
-            return (salario * 0.075) - 142.80;
+            return ((salario * 0.075) - 142.80).toFixed(2) + " reais.";
         }
         if(salario > 2826.65 && salario <= 3751.05)
         {
-            return (salario * 0.15) - 354.80;
+            return ((salario * 0.15) - 354.80).toFixed(2) + " reais.";
         }
         if(salario > 3751.05 && salario <= 4664.68)
         {
-            return (salario * 0.225) - 636.13;
+            return ((salario * 0.225) - 636.13).toFixed(2) + " reais.";
         }
         if(salario > 4664.68)
         {
-            return (salario * 0.275) - 869.36;
+            return ((salario * 0.275) - 869.36).toFixed(2) + " reais.";
         }
     }
 
